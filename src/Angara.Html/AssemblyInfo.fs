cf9b512765fc224de0ac19a44149a6e039a628db
ElenaPochernina <elenap@itis.cs.msu.ru>
2017-04-06 12:50:36 +0300

    update Angara.Chart and Angara.Table

--- src/Angara.Html/AssemblyInfo.fs
+++ src/Angara.Html/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Angara.Html")>]
 [<assembly: AssemblyProductAttribute("Angara")>]
 [<assembly: AssemblyDescriptionAttribute("The modelling environment")>]
-[<assembly: AssemblyVersionAttribute("0.2.5")>]
-[<assembly: AssemblyFileVersionAttribute("0.2.5")>]
+[<assembly: AssemblyVersionAttribute("0.2.6")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.6")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.2.5"
-    let [<Literal>] InformationalVersion = "0.2.5"
+    let [<Literal>] Version = "0.2.6"
+    let [<Literal>] InformationalVersion = "0.2.6"
