7ac741685ba2e68041ff2f7c92dfc00a8184c2e2
Dmitry Voytsekhovskiy <dvoits@gmail.com>
2018-07-04 00:48:16 +0300

    Referencing Itis.Angara.Table

--- src/Angara.Html.Sample/AssemblyInfo.fs
+++ src/Angara.Html.Sample/AssemblyInfo.fs
@@ -3,15 +3,15 @@ namespace System
 open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Angara.Html.Sample")>]
-[<assembly: AssemblyProductAttribute("Angara")>]
+[<assembly: AssemblyProductAttribute("Itis.Angara")>]
 [<assembly: AssemblyDescriptionAttribute("The modelling environment")>]
-[<assembly: AssemblyVersionAttribute("0.3.2")>]
-[<assembly: AssemblyFileVersionAttribute("0.3.2")>]
+[<assembly: AssemblyVersionAttribute("0.3.3")>]
+[<assembly: AssemblyFileVersionAttribute("0.3.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Angara.Html.Sample"
-    let [<Literal>] AssemblyProduct = "Angara"
+    let [<Literal>] AssemblyProduct = "Itis.Angara"
     let [<Literal>] AssemblyDescription = "The modelling environment"
-    let [<Literal>] AssemblyVersion = "0.3.2"
-    let [<Literal>] AssemblyFileVersion = "0.3.2"
+    let [<Literal>] AssemblyVersion = "0.3.3"
+    let [<Literal>] AssemblyFileVersion = "0.3.3"
--- src/Angara.Reinstate/AssemblyInfo.fs
+++ src/Angara.Reinstate/AssemblyInfo.fs
@@ -3,15 +3,15 @@ namespace System
 open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Angara.Reinstate")>]
-[<assembly: AssemblyProductAttribute("Angara")>]
+[<assembly: AssemblyProductAttribute("Itis.Angara")>]
 [<assembly: AssemblyDescriptionAttribute("The modelling environment")>]
-[<assembly: AssemblyVersionAttribute("0.3.2")>]
-[<assembly: AssemblyFileVersionAttribute("0.3.2")>]
+[<assembly: AssemblyVersionAttribute("0.3.3")>]
+[<assembly: AssemblyFileVersionAttribute("0.3.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Angara.Reinstate"
-    let [<Literal>] AssemblyProduct = "Angara"
+    let [<Literal>] AssemblyProduct = "Itis.Angara"
     let [<Literal>] AssemblyDescription = "The modelling environment"
-    let [<Literal>] AssemblyVersion = "0.3.2"
-    let [<Literal>] AssemblyFileVersion = "0.3.2"
+    let [<Literal>] AssemblyVersion = "0.3.3"
+    let [<Literal>] AssemblyFileVersion = "0.3.3"
